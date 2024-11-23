835f025dd881eacb2e0ca078ed6d600b01b0f397
SyedFarmaan <163553630+SyedFarmaan@users.noreply.github.com>
2024-11-23 12:28:31 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,1 +1,9 @@
+def prime(num):
+  print("The given number is prime")
+  print("this function will identify whether the given number is prime or not")
+  if num == 1:
+    print("this is a prime number")
+  
 
+pr = prime(1)
+  
